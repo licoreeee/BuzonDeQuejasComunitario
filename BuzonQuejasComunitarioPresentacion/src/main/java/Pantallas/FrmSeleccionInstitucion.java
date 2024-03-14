78326c4670499c3dbfcfe91ac3ba3669a012fd32
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-03-14 00:10:20 -0700

    Multimediaa

--- BuzonQuejasComunitarioPresentacion/src/main/java/Pantallas/FrmSeleccionInstitucion.java
+++ BuzonQuejasComunitarioPresentacion/src/main/java/Pantallas/FrmSeleccionInstitucion.java
@@ -27,19 +27,51 @@ public class FrmSeleccionInstitucion extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        lblLogoGobiernoMexico = new javax.swing.JLabel();
+        lblDescripcion1 = new javax.swing.JLabel();
+        lblDescripcion2 = new javax.swing.JLabel();
+        btnLevantarReporte = new javax.swing.JButton();
+        btnAvances = new javax.swing.JButton();
+        btnSiguiente = new javax.swing.JButton();
+        btnHistorialAyuda = new javax.swing.JButton();
         cboSeleccionInstitucion = new javax.swing.JComboBox<>();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        lblTitulo = new javax.swing.JLabel();
+        lblHeader = new javax.swing.JLabel();
+        lblContenido = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setPreferredSize(new java.awt.Dimension(600, 360));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("Siguiente");
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 300, 100, 40));
+        lblLogoGobiernoMexico.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Pantallas/multimedia/logo_gobierno_mexico.png"))); // NOI18N
+        jPanel1.add(lblLogoGobiernoMexico, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        lblDescripcion1.setForeground(new java.awt.Color(0, 0, 0));
+        lblDescripcion1.setText("Seleccione entre las instituciones disponibles las cuales considere son las indicadas para darle ");
+        jPanel1.add(lblDescripcion1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 510, 20));
+
+        lblDescripcion2.setForeground(new java.awt.Color(0, 0, 0));
+        lblDescripcion2.setText("seguimiento al reporte.");
+        jPanel1.add(lblDescripcion2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, 500, -1));
+
+        btnLevantarReporte.setBackground(new java.awt.Color(10, 50, 6));
+        btnLevantarReporte.setText("Levantar Reporte");
+        jPanel1.add(btnLevantarReporte, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 50, 130, -1));
+
+        btnAvances.setBackground(new java.awt.Color(10, 50, 6));
+        btnAvances.setText("Avances");
+        jPanel1.add(btnAvances, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 50, -1, -1));
+
+        btnSiguiente.setBackground(new java.awt.Color(229, 229, 229));
+        btnSiguiente.setForeground(new java.awt.Color(181, 18, 57));
+        btnSiguiente.setText("Siguiente");
+        btnSiguiente.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(181, 18, 57), 3, true));
+        jPanel1.add(btnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 340, 100, 40));
+
+        btnHistorialAyuda.setBackground(new java.awt.Color(10, 50, 6));
+        btnHistorialAyuda.setText("Historial Ayudas");
+        jPanel1.add(btnHistorialAyuda, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 50, -1, -1));
 
         cboSeleccionInstitucion.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "CFE", "OOMAPAS", "Policia Municipal de Cajeme", "Residuos Sólidos Urbanos", "IMCYC" }));
         cboSeleccionInstitucion.addActionListener(new java.awt.event.ActionListener() {
@@ -47,16 +79,20 @@ public class FrmSeleccionInstitucion extends javax.swing.JFrame {
                 cboSeleccionInstitucionActionPerformed(evt);
             }
         });
-        jPanel1.add(cboSeleccionInstitucion, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 150, 250, 40));
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 180, 70));
+        jPanel1.add(cboSeleccionInstitucion, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 210, 250, 40));
 
-        jLabel1.setBackground(new java.awt.Color(0, 102, 51));
-        jLabel1.setOpaque(true);
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 600, 80));
+        lblTitulo.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblTitulo.setForeground(new java.awt.Color(0, 0, 0));
+        lblTitulo.setText("Instituciones Correspondientes");
+        jPanel1.add(lblTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 100, -1, -1));
 
-        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel3.setOpaque(true);
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 600, 280));
+        lblHeader.setBackground(new java.awt.Color(10, 50, 6));
+        lblHeader.setOpaque(true);
+        jPanel1.add(lblHeader, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 600, 80));
+
+        lblContenido.setBackground(new java.awt.Color(229, 229, 229));
+        lblContenido.setOpaque(true);
+        jPanel1.add(lblContenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 600, 320));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -66,9 +102,7 @@ public class FrmSeleccionInstitucion extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
 
         pack();
@@ -115,11 +149,17 @@ public class FrmSeleccionInstitucion extends javax.swing.JFrame {
 //    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAvances;
+    private javax.swing.JButton btnHistorialAyuda;
+    private javax.swing.JButton btnLevantarReporte;
+    private javax.swing.JButton btnSiguiente;
     private javax.swing.JComboBox<String> cboSeleccionInstitucion;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblContenido;
+    private javax.swing.JLabel lblDescripcion1;
+    private javax.swing.JLabel lblDescripcion2;
+    private javax.swing.JLabel lblHeader;
+    private javax.swing.JLabel lblLogoGobiernoMexico;
+    private javax.swing.JLabel lblTitulo;
     // End of variables declaration//GEN-END:variables
 }
