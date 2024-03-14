f3a0e048bdd689b48a79dd090fd2f15b90b2b637
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-03-14 00:52:18 -0700

    errores

---
+++ BuzonQuejasComunitarioPresentacion/src/main/java/Pantallas/Pruebas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pantallas;
+
+/**
+ *
+ * @author castr
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmSeleccionInstitucion selecInstitucion = new FrmSeleccionInstitucion();
+        selecInstitucion.setVisible(true);
+    }
+    
+}
