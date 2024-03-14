2fd394db2adc91ee5e6c637580016a408682592e
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-03-13 21:31:42 -0700

    clonado

--- BuzonQuejasComunitarioPresentacion/src/main/java/Pantallas/MenuPrincipal.java
+++ BuzonQuejasComunitarioPresentacion/src/main/java/Pantallas/MenuPrincipal.java
@@ -30,7 +30,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Hola");
+        jLabel1.setText("hola :)");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -39,7 +39,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(162, 162, 162)
                 .addComponent(jLabel1)
-                .addContainerGap(213, Short.MAX_VALUE))
+                .addContainerGap(205, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
