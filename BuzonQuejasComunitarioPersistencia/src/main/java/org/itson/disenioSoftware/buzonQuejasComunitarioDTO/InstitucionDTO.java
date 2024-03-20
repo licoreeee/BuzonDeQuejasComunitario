8eb158afe17bcf6bbd2110fdf24d5be73db51551
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-03-19 19:32:59 -0700

    DTOs

---
+++ BuzonQuejasComunitarioPersistencia/src/main/java/org/itson/disenioSoftware/buzonQuejasComunitarioDTO/InstitucionDTO.java
@@ -0,0 +1,44 @@
+
+package org.itson.disenioSoftware.buzonQuejasComunitarioDTO;
+
+
+public class InstitucionDTO {
+    private int idInstitucion;
+    private String nombreInstitucion;
+    private String funcionInstitucion;
+    private String siglas;
+
+    public int getIdInstitucion() {
+        return idInstitucion;
+    }
+
+    public void setIdInstitucion(int idInstitucion) {
+        this.idInstitucion = idInstitucion;
+    }
+
+    public String getNombreInstitucion() {
+        return nombreInstitucion;
+    }
+
+    public void setNombreInstitucion(String nombreInstitucion) {
+        this.nombreInstitucion = nombreInstitucion;
+    }
+
+    public String getFuncionInstitucion() {
+        return funcionInstitucion;
+    }
+
+    public void setFuncionInstitucion(String funcionInstitucion) {
+        this.funcionInstitucion = funcionInstitucion;
+    }
+
+    public String getSiglas() {
+        return siglas;
+    }
+
+    public void setSiglas(String siglas) {
+        this.siglas = siglas;
+    }
+    
+    
+}
---
+++ BuzonQuejasComunitarioPersistencia/src/main/java/org/itson/disenioSoftware/buzonQuejasComunitarioDTO/ReporteDTO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.disenioSoftware.buzonQuejasComunitarioDTO;
+
+
+public class ReporteDTO {
+     private int folio;
+    private String titulo;
+    private IncidenteDTO incidente;
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public IncidenteDTO getIncidente() {
+        return incidente;
+    }
+
+    public void setIncidente(IncidenteDTO incidente) {
+        this.incidente = incidente;
+    }
+    
+    
+    
+}
