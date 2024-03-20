23ddaffdc1fec645d3b7ae37c4fc06ea3cc8743d
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-03-19 19:59:55 -0700

    Clase de negocios

---
+++ BuzonQuejasComunitarioPersistencia/src/main/java/org/itson/disenioSoftware/buzonQuejasComunitarioAdministrarAcceso/IAdministrarAcceso.java
@@ -0,0 +1,9 @@
+
+package org.itson.disenioSoftware.buzonQuejasComunitarioAdministrarAcceso;
+
+import org.itson.disenioSoftware.buzonQuejasComunitarioDTO.InstitucionDTO;
+
+
+public interface IAdministrarAcceso {
+    public void agregarInstitucion(InstitucionDTO institucionDTO);
+}
