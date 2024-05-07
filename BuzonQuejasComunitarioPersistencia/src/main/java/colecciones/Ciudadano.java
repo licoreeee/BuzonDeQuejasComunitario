91209a4e93610f704f1ef818143d6f6848d9e614
Victoria Vega <victoria.vega247804@potros.itson.edu.mx>
2024-05-07 03:51:09 -0700

    Comienzo de conexión con Mongo
    Ya puse las clases de las colecciones reporte, institución y ciudadano

---
+++ BuzonQuejasComunitarioPersistencia/src/main/java/colecciones/Ciudadano.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colecciones;
+
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author Dell
+ */
+public class Ciudadano {
+
+    private ObjectId id;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String curp;
+    private String numeroTelefono;
+    private String correo;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+}
+
---
+++ BuzonQuejasComunitarioPersistencia/src/main/java/colecciones/Reporte.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colecciones;
+
+import java.util.Calendar;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author Dell
+ */
+public class Reporte {
+
+    private ObjectId id;
+    private int folio;
+    private String titulo;
+    private String descripcion;
+    private Calendar fechaCreacion;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public Calendar getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Calendar fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+    
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
+
