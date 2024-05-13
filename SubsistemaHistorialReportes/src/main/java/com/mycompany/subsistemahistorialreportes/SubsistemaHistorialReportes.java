6d5b474d1e4a3515819576545e96fd139fe2a949
Victoria Vega <159996124+licoreeee@users.noreply.github.com>
2024-05-13 01:14:52 -0700

    Creación proyecto SubsistemaHistorialReportes

---
+++ SubsistemaHistorialReportes/src/main/java/com/mycompany/subsistemahistorialreportes/SubsistemaHistorialReportes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.subsistemahistorialreportes;
+
+/**
+ *
+ * @author Dell
+ */
+public class SubsistemaHistorialReportes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
