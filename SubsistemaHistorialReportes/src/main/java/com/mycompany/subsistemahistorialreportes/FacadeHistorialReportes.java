a126c267a13c91f6d2b4eb8ef9976307486d4615
Victoria Vega <159996124+licoreeee@users.noreply.github.com>
2024-05-13 17:59:45 -0700

    Eliminé main de este Frame

--- SubsistemaHistorialReportes/src/main/java/com/mycompany/subsistemahistorialreportes/FacadeHistorialReportes.java
+++ SubsistemaHistorialReportes/src/main/java/com/mycompany/subsistemahistorialreportes/FacadeHistorialReportes.java
@@ -19,7 +19,7 @@ public class FacadeHistorialReportes implements IFacadeHistorialReportes{
     
     private HistorialReportes historial;
 
-    public FacadeHistorialReportes(HistorialReportes historial) {
+    public FacadeHistorialReportes() {
         historial = new HistorialReportes();
     }
 
